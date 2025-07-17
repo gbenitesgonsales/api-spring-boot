38e0c68984a68c6f23ac1b20a66be73f6ba8a291
Gabriel Benites Gonsales <gbenitesgonsales@gmail.com>
2025-07-16 23:30:50 -0300

    feat: adicionar suporte a variáveis de ambiente com dotenv e nova rota de saudação

--- src/main/java/com/gbenitesgonsales/apijava/Main.java
+++ src/main/java/com/gbenitesgonsales/apijava/Main.java
@@ -4,13 +4,22 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @RestController
 @SpringBootApplication
 public class Main {
+
     @GetMapping("/hello")
     public String hello(){
         return "Hello, World!" ;
+    }
+
+    @GetMapping("/greet")
+    public String greet(){
+        Dotenv dotenv = Dotenv.load();
+        String name = dotenv.get("NAME");
+        return "Hello, "+name+"!" ;
     }
 
     public static void main(String[] args) {
